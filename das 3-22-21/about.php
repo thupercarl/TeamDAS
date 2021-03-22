b1f31de7bd612179daba63ed737d640d3ec26db3
Jake Donaldson <77291427+thupercarl@users.noreply.github.com>
2021-03-22 15:29:40 -0700

    Add files via upload

---
+++ das 3-22-21/about.php
@@ -0,0 +1,68 @@
+<!--
+Name: Gavin Sherman
+Date: 01-24-2021
+
+This is the about.html page for the Coneybeare Cleantech website. It links to the about, home, and sign up page
+and it displays the information about Coneybeare Cleantech and its president.
+-->
+
+<?php
+include('includes/head.php');
+?>
+<link rel="stylesheet" href="css/about.css">
+<body>
+<div class="container bg-light" id="main">
+
+    <?php
+    include('includes/navbar.php');
+    ?>
+
+    <div>
+        <h1>About Us</h1>
+        <p>
+            Coneybeare Cleantech is a full-service recruitment leader in the sustainable technology and renewable energy
+            sectors, offering retained and exclusive contingency recruiting, direct hire and contractor placements,
+            consulting and training services as well as reference checks.
+            Employers may hire us to connect them with great employees, independent contractors, or consultants.
+            Our consultants also offer training in both employment and sustainability-related practices.
+            Coneybeare Cleantech’s team of recruiters, who have built a robust employer and candidate network,
+            produces superior results and makes the process of finding and placing premier talent seamless.
+
+        </p>
+        <p>
+            Our team of dedicated, talented recruiters provides world-class services customized to each client’s
+            individual needs, whether they are a start-up company or Fortune 500 corporation.
+        </p>
+        <p>
+            Headquartered in Santa Ana, Calif., the organization has offices in San Francisco and Colombia, South America.
+        </p>
+        <p>
+            Building a More Sustainable Future
+        </p>
+        <p>
+            Beyond its recruitment and consulting services, Coneybeare Cleantech is a leader in the clean technology movement.
+            Lead by its founder Victoria Betancourt, our mission is to build a more sustainable future for the coming
+            generations through a mix of innovation, public policy and private sector investment.
+        </p>
+        <p>
+            Ultimately, recruiting is all about connecting people.
+            Exchanging ideas and forging relationships that serve this goal are the core values of Coneybeare Cleantech.
+        </p>
+    </div>
+    <div class="owner">
+        <img src="images/Coneybeare%20owner.png" alt="logo">
+        <p class="owner">President</p>
+        <p class="owner">Victoria Betancourt</p>
+    </div>
+
+    <?php
+    include('includes/footer.html');
+    ?>
+
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ das 3-22-21/confirm.php
@@ -0,0 +1,304 @@
+<!--
+Name: Jean-Kenneth Antonio
+Date: 01-24-2021
+
+This is the confirm.php page for the Coneybeare Cleantech website. It links to the about, home, and sign up page
+and displays form data submitted from webform.html
+-->
+<?php
+
+if (!($_SERVER['REQUEST_METHOD'] == 'POST')) {
+    echo "console.log('If request method fired')";
+    //send user to form page
+    header("Location: webform.php");
+}
+
+include('includes/head.php');
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+?>
+
+<body>
+
+<div class="container-xl bg-light" id="main">
+
+    <!--Navigation bar-->
+    <?php
+    include('includes/navbar.php');
+    ?>
+
+    <!-- Thank you -->
+    <br>
+    <div class="row">
+        <div class="text-center offset-1 h4">
+            Thank you, your information has been submitted for review.
+        </div>
+    </div>
+
+    <br>
+
+    <!-- Submission Summary -->
+    <div class="container border p-3 theme-color">
+
+        <!-- Form data-->
+
+        <div class="border p-2 bg-white p-5">
+
+            <?php
+            $cnxn = connect();
+            //company information var
+            $cname = mysqli_real_escape_string($cnxn, $_POST['cname']);
+            $cabout = mysqli_real_escape_string($cnxn, $_POST['cabout']);
+            $ctagline = mysqli_real_escape_string($cnxn, $_POST['ctagline']);
+            $cemail = mysqli_real_escape_string($cnxn, $_POST['cemail']);
+            $cphone = mysqli_real_escape_string($cnxn, $_POST['cphone']);
+            $curl = mysqli_real_escape_string($cnxn, $_POST['curl']);
+            $ccategory = mysqli_real_escape_string($cnxn, $_POST['ccategory']);
+            //$clogo = $_POST['clogo'];
+            $keywords = mysqli_real_escape_string($cnxn, $_POST['keywords']);
+
+            //Contact person var
+            $cpfname = mysqli_real_escape_string($cnxn, $_POST['cpfname']);
+            $cplname = mysqli_real_escape_string($cnxn, $_POST['cplname']);
+            $cpphone = mysqli_real_escape_string($cnxn, $_POST['cpphone']);
+            $cpemail = mysqli_real_escape_string($cnxn, $_POST['cpemail']);
+
+            //location var
+            $addressl1 = mysqli_real_escape_string($cnxn, $_POST['addressl1']);
+            $addressl2 = mysqli_real_escape_string($cnxn, $_POST['addressl2']);
+            $steapt = mysqli_real_escape_string($cnxn, $_POST['ste&apt']);
+            $czip = mysqli_real_escape_string($cnxn, $_POST['czip']);
+
+            $city = mysqli_real_escape_string($cnxn, $_POST['city']);
+            $state = mysqli_real_escape_string($cnxn, $_POST['state']);
+            $country = mysqli_real_escape_string($cnxn, $_POST['country']);
+            $carea = mysqli_real_escape_string($cnxn, $_POST['carea']);
+
+            //echo var_dump($keywords);
+            //$keywords = explode(",", $keywords);
+            //echo var_dump($keywords);
+
+            //IMAGE UPLOAD
+            $target_dir = "uploads/";
+            $target_file = $target_dir . basename($_FILES["clogo"]["name"]);
+            $uploadOk = 1;
+            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+            // Check if image file is a actual image or fake image
+            if(isset($_POST["submit"])) {
+                $check = getimagesize($_FILES["clogo"]["tmp_name"]);
+                if($check !== false) {
+                    //echo "File is an image - " . $check["mime"] . ".";
+                    $uploadOk = 1;
+                } else {
+                    //echo "File is not an image.";
+                    $uploadOk = 0;
+                }
+            }
+
+            // Check if file already exists
+            if (file_exists($target_file)) {
+                //echo "Sorry, file already exists.";
+                $uploadOk = 0;
+            }
+
+            // Check file size
+            if (($_FILES["clogo"]["size"])/1024/1024 > 5) {
+                //echo "Sorry, your file is too large.";
+                $uploadOk = 0;
+            }
+
+            // Allow certain file formats
+            if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+                && $imageFileType != "gif" ) {
+                //echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                $uploadOk = 0;
+            }
+
+            // Check if $uploadOk is set to 0 by an error
+            if ($uploadOk == 0) {
+                //echo "Sorry, your file was not uploaded.";
+            // if everything is ok, try to upload file
+            } else {
+                if (move_uploaded_file($_FILES["clogo"]["tmp_name"], $target_file)) {
+                    //echo "The file ". htmlspecialchars( basename( $_FILES["clogo"]["name"])). " has been uploaded.";
+                } else {
+                    echo "Sorry, there was an error uploading your file.";
+                }
+            }
+
+            //file name var
+            //echo $_FILES["clogo"]["name"];
+            $filename = $target_dir . $_FILES["clogo"]["name"];
+
+
+            //Email start
+            //Email body will fill in as it is submitted
+            $emailBody = "A request has been made<br>";
+
+            //submission summary
+            echo "<h5 class='h5'> Submission Summary</h5>";
+            echo "<b>Company Name: </b> $cname <br>";
+
+            $emailBody .= "<b>Company Name: </b> $cname <br>";
+
+            if (!empty($cabout)) {
+                echo "<b>Company About: </b> $cabout <br>";
+
+                $emailBody .= "<b>Company About: </b> $cabout <br>";
+
+                if (!empty($ctagline)) {
+                    echo "<b>Tagline: </b> $ctagline <br>";
+                    $emailBody .= "<b>Tagline: </b> $ctagline <br>";
+                }
+            }
+
+            //show company email if not empty
+            if (!empty($cemail)){
+                echo "<b>Company Email: </b> $cemail <br>";
+                $emailBody .= "<b>Company Email: </b> $cemail <br>";
+            }
+
+            //show company phone if not empty
+            if (!empty($cphone)) {
+                echo "<b>Company Phone: </b> $cphone <br>";
+                $emailBody .= "<b>Company Phone: </b> $cphone <br>";
+            }
+
+            //show categories
+            echo "<b>Company Category: </b> $ccategory <br>";
+            $emailBody .= "<b>Company Category: </b> $ccategory <br>";
+
+            //Image Provided
+            //used inline styling here. Couldn't get to work with external sheet
+            echo "<b>Image Provided: </b> <br> <img src='" . $filename . "' alt='Provided Image' style='max-width: 150px'> <br>";
+            $emailBody .= "<b>Image Provided: </b> <br> <img src='https://das.greenriverdev.com/" . $filename . "' alt='Provided Image' style='max-width: 150px'> <br>";
+
+            //Keywords
+            echo "<b>Keywords: </b> $keywords <br> <br>";
+            $emailBody .= "<b>Keywords: </b> $keywords <br> <br>";
+
+            //point of contact info
+
+            //Contact name
+            if (!empty($cpfname) && !empty($cplname)) {
+                echo "<b>Contact Name: </b> $cpfname $cplname <br>";
+                $emailBody .= "<b>Contact Name: </b> $cpfname $cplname <br>";
+            }
+
+            //contact phone number
+            echo "<b>Contact Phone: </b> $cpphone <br> <br>";
+            $emailBody .="<b>Contact Phone: </b> $cpphone <br> <br>";
+
+            //contact email
+            echo "<b>Contact Email: </b> $cpemail <br> <br>";
+            $emailBody .="<b>Contact Email: </b> $cpemail <br> <br>";
+
+            //location info echo
+            echo "<b>Location: </b><br>";
+
+            //Address line 1
+            if (!empty($addressl1)) {
+                echo "$addressl1";
+                $emailBody .= "$addressl1";
+            }
+
+            //Address line 2
+            if (!empty($addressl2)) {
+                echo "<br>$addressl2";
+                $emailBody .= "<br>$addressl2";
+            }
+
+            //suite or apt or unit
+            if (!empty($steapt)) {
+                echo " $steapt";
+                $emailBody .= " $steapt";
+            }
+
+            //city and state
+            echo "<br>$city $state";
+            $emailBody .= "<br>$city $state";
+
+            //zip code
+            if (!empty($czip)) {
+                echo " $czip";
+                $emailBody .= " $czip";
+            }
+
+            //country abbreviation
+            echo "<br>$country";
+            $emailBody .= "<br>$country";
+
+
+            echo "<br>";
+            $emailBody .= "<br>";
+
+            //Service area
+            echo "<b>Service Area: </b> $carea <br> <br>";
+            $emailBody .= "<b>Service Area: </b> $carea <br> <br>";
+
+            //concatenate for database entry
+            $address = $addressl1 . " " . $addressl2 . " " . $steapt;
+            $clogo = "https://das.greenriverdev.com/".$target_file;
+            //send to database
+            $sql = "INSERT INTO companies (name, category, logo, about, tagline, url, 
+                       tag_cloud, company_phone, company_email, contact_fname, contact_lname, contact_phone, 
+                       contact_email, address, city, state, country) 
+                    VALUES ('$cname', '$ccategory', '$clogo', 
+                            '$cabout', '$ctagline', '$curl', '$keywords', '$cphone', '$cemail', '$cpfname',
+                            '$cplname', '$cpphone', '$cpemail', '$address', '$city', '$state', '$country');";
+
+            //sql id
+            $id = mysqli_insert_id($cnxn);
+
+            //URL
+            //$url = "https://jantonio.greenriverdev.com/305/pizza/view.php?id=$id";
+
+
+            //Send email to admin
+            $emailTo = 'coneybearecatalog@gmail.com';
+            $emailFrom = 'Coneybeare Cleantech <coneybearecatalog@gmail.com>';
+
+
+            //Finish up the email body
+            $emailSubject = 'New Catalog Request';
+            $headers = "Content-type: text/html; charset=UTF-8\r\n";
+            $headers .= "From: $emailFrom\r\n";
+            $emailBody .= "Company ID: $id";
+            $emailBody .= "<br> Login here to review request: <br> https://das.greenriverdev.com/login.php";
+            //$success = mail($emailTo, $emailSubject, $emailBody, $headers);
+
+
+
+
+
+            $success = mysqli_query($cnxn, $sql);
+
+
+            ?>
+
+        </div>
+
+    </div>
+    <br>
+    <br>
+
+    <!-- Footer -->
+    <?php
+        include('includes/footer.html');
+    ?>
+</div>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<!--Webform country select maintained by mojoaxel: https://mojoaxel.github.io/bootstrap-select-country/index.html -->
+<script src="js/bootstrap-select-country.min.js"></script>
+
+</body>
+</html>
