a050ee159d4b1f15f8d77189f4bccb77ccffb0ec
Jake Donaldson <77291427+thupercarl@users.noreply.github.com>
2021-03-23 11:29:53 -0700

    Add files via upload

---
+++ das Sprint 5 Final Site/includes/homepagehead.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/coneybearestyles.css">
+    <!--Favicon-->
+    <link href="images/coneybeare-2-transparent.png" type="image/png" rel="icon">
+    <title>ConeyBeare Sustainability Catalogue</title>
+</head>
